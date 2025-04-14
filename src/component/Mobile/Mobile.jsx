537d4e58486632deac8d53cffb0a782f18f482bd
Mokarama <mokaramaaktershanta@gmail.com>
2025-04-14 09:28:24 +0600

    Installatin react-router-dom & Tailwind CSS

---
+++ src/component/Mobile/Mobile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Mobile = () => {
+    return (
+        <div>
+            <h2>Mobile:</h2>
+            <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+        </div>
+    );
+};
+
+export default Mobile;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Mobile from './component/Mobile/Mobile.jsx';
+
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Mobile></Mobile>
+    
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
