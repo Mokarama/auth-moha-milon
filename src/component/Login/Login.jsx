e83707c347f09c8f6637ac1415234faccb9d8cee
Mokarama <mokaramaaktershanta@gmail.com>
2025-04-14 17:09:21 +0600

    Login Form & Register Form added

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handleLogin =(e)=>{
+        e.preventDefault();
+        const email =e.target.email.value;
+        const password =e.target.password.value;
+        console.log(email, password)
+    }
     return (
-      <div>
-        <h2>Login</h2>
+        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-2xl font-bold">Login now!</h1>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+             <form onSubmit={handleLogin}> 
+             <fieldset className="fieldset">
+                <label className="fieldset-label">Email</label>
+                <input name='email' type="email" className="input" placeholder="Email" />
+                <label className="fieldset-label">Password</label>
+                <input type="password" name='password'  className="input" placeholder="Password" />
+                <div><a className="link link-hover">Forgot password?</a></div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+              
+             </form>
+             <p>New to this website?Please <Link to="/register">Register</Link></p>
+            </div>
+          </div>
+        </div>
       </div>
     );
 };
--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const handleRegister =(e) =>{
+        e.preventDefault();
+        const email =e.target.email.value;
+        const password =e.target.email.value;
+        console.log(email, password)
+    }
     return (
-        <div>
-            <h2>Register</h2>
+        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-2xl font-bold">Register now!</h1>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+             <form onSubmit={handleRegister}> 
+             <fieldset className="fieldset">
+                <label className="fieldset-label">Email</label>
+                <input name='email' type="email" className="input" placeholder="Email" />
+                <label className="fieldset-label">Password</label>
+                <input type="password" name='password'  className="input" placeholder="Password" />
+                <div><a className="link link-hover">Forgot password?</a></div>
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+              
+             </form>
+             <p>New to this website?Please <Link to="/login">Login</Link></p>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
