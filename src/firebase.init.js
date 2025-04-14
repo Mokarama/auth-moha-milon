c180a2c49a27374b4cf91a06487b848f6d7ec99b
Mokarama <mokaramaaktershanta@gmail.com>
2025-04-14 10:21:36 +0600

    navbar added

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGGZZNA8xAJJognRObYe9TcYyXLRyFYqE",
+  authDomain: "auth-moha-milon-ef806.firebaseapp.com",
+  projectId: "auth-moha-milon-ef806",
+  storageBucket: "auth-moha-milon-ef806.firebasestorage.app",
+  messagingSenderId: "57599723596",
+  appId: "1:57599723596:web:98b84daa2a136d080fcce9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Mobile from './component/Mobile/Mobile.jsx';
+// import Mobile from './component/Mobile/Mobile.jsx';
+import Root from './component/Root/Root.jsx';
+import Home from './component/Home/Home.jsx';
+import Login from './component/Login/Login.jsx';
+import Register from './component/Register/Register.jsx';
 
 
 let router = createBrowserRouter([
   {
     path: "/",
-    element:<Mobile></Mobile>
+    element:<Root></Root>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+
+      {
+        path:"login",
+        element:<Login></Login>
+      },
+
+      {
+        path:"register",
+        element:<Register></Register>
+      }
+    ]
     
   },
 ]);
